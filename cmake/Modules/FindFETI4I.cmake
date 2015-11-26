e467934c4142f9879a69cd5420c708576b1c01ac
Vaclav Hapla <vaclav.hapla@vsb.cz>
2015-11-26 10:55:00 +0100

    CMake takes FETI4I_ROOT variable and links to FETI4I from this dir

--- cmake/Modules/FindFETI4I.cmake
+++ cmake/Modules/FindFETI4I.cmake
@@ -7,11 +7,15 @@ SET(_feti4iLibName "libfeti4i.so")
 # If MKL_LIBRARIES libraries are already defined, do nothing
 SET(FETI4I_FOUND FALSE)
 
-SET(_feti4iIncludePaths
-  "$ENV{FETI4I_ROOT}/include"
-  "${FETI4I_ROOT}/include"
-  INTERNAL
-)
+IF (NOT FETI4I_ROOT)
+  SET(FETI4I_ROOT "$ENV{FETI4I_ROOT}")
+ENDIF()
+
+#SET(_feti4iIncludePaths
+#  "$ENV{FETI4I_ROOT}/include"
+#  "${FETI4I_ROOT}/include"
+#  INTERNAL
+#)
 
 SET(_feti4iLibPaths
   "$ENV{FETI4I_ROOT}/lib"
@@ -19,28 +23,26 @@ SET(_feti4iLibPaths
   INTERNAL
 )
 
+#TODO different subdir
 SET(_feti4iInterfaceSrcPaths
-  "ENV{FETI4I_ROOT}/lib/${_feti4iIfaceF90}"
-  "${FETI4I_ROOT}/lib/${_feti4iIfaceF90}"
+  "$ENV{FETI4I_ROOT}/${_feti4iIfaceF90}"
+  "${FETI4I_ROOT}/${_feti4iIfaceF90}"
   INTERNAL
 )
 
 # Find Feti4i library
 #FIND_LIBRARY(FETI4I_LIBRARIES ${_feti4iLibName}${SHL_EXTENSION} HINTS ${_feti4iLibPaths})
-
-SET(FETI4I_LIBRARIES ${PROJECT_SOURCE_DIR}/feti4i/feti4i_fortran_test/lib/${_feti4iLibName} CACHE FILE "")
-SET(FETI4I_INTERFACE_SOURCE ${PROJECT_SOURCE_DIR}/feti4i/${_feti4iIfaceF90} CACHE FILE "")
+#SET(FETI4I_LIBRARIES ${PROJECT_SOURCE_DIR}/feti4i/feti4i_fortran_test/lib/${_feti4iLibName} CACHE FILE "")
+SET(FETI4I_LIBRARIES "${FETI4I_ROOT}/lib/${_feti4iLibName}" CACHE FILE "")
 
 # Find the actual interface file
 #FIND_FILE(FETI4I_INTERFACE_SOURCE NAMES ${_feti4iIfaceF90} PATHS ${_feti4iInterfaceSrcPaths})
+#SET(FETI4I_INTERFACE_SOURCE ${PROJECT_SOURCE_DIR}/feti4i/${_feti4iIfaceF90} CACHE FILE "")
+SET(FETI4I_INTERFACE_SOURCE "${FETI4I_ROOT}/${_feti4iIfaceF90}" CACHE FILE "")
 
-#IF(_feti4iLibrary AND _feti4iInterfaceSrc)
-#  UNSET(FETI4I_FAILMSG)
-#  SET(FETI4I_LIBRARIES ${_feti4iLibrary} CACHE FILEPATH "Feti4i library")
-#  SET(FETI4I_INTERFACE_SOURCE ${_feti4iInterfaceSrc} CACHE FILEPATH "Feti4i interface source")
-#ELSE()
-#  SET(FETI4I_FAILMSG "Feti4i not found")
-#ENDIF()
+message(STATUS "FETI4I_LIBRARIES=${FETI4I_LIBRARIES}")
+message(STATUS "FETI4I_INTERFACE_SOURCE=${FETI4I_INTERFACE_SOURCE}")
+
 
 IF(FETI4I_LIBRARIES AND FETI4I_INTERFACE_SOURCE)
   SET(FETI4I_FOUND TRUE)
